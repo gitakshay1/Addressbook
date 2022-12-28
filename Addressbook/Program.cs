52fd5939885eb3eedb225f80fa596328f66fb5da
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-28 16:53:48 +0530

    [Akshay]Add:Added Program to create basic contact book

---
+++ Addressbook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Addressbook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook addressBook = new AddressBook("Akshay","Patil","AntorePhata","Pen","Maharashtra","akshaypatil309p8@gamil.com",402107,98587456985);
+            addressBook.DisplayContatcs();
+        }
+    }
+}
